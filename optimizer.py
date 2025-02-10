35045e62de05579e1b0124a4fe5560cece65b65b
xx <xiaoxu1094@gmail.com>
2025-02-10 14:59:30 -0500

    Pass optimizer

--- optimizer.py
+++ optimizer.py
@@ -13,7 +13,7 @@ class AdamW(Optimizer):
             eps: float = 1e-6,
             weight_decay: float = 0.0,
             correct_bias: bool = True,
-            max_grad_norm: float = None,
+            max_grad_norm: float = None,  # type: ignore
     ):
         if lr < 0.0:
             raise ValueError("Invalid learning rate: {} - should be >= 0.0".format(lr))
@@ -29,7 +29,7 @@ class AdamW(Optimizer):
                         correct_bias=correct_bias, max_grad_norm=max_grad_norm)
         super().__init__(params, defaults)
 
-    def step(self, closure: Callable = None):
+    def step(self, closure: Callable = None):  # type: ignore
         loss = None
         if closure is not None:
             loss = closure()
@@ -38,33 +38,49 @@ class AdamW(Optimizer):
 
             # TODO: Clip gradients if max_grad_norm is set
             if group['max_grad_norm'] is not None:
-                raise NotImplementedError()
+                torch.nn.utils.clip_grad_norm_(group['params'],  # type: ignore
+                                               group['max_grad_norm'])
 
             for p in group["params"]:
                 if p.grad is None:
                     continue
                 grad = p.grad.data
+                theta = p.data
+
                 if grad.is_sparse:
                     raise RuntimeError(
                         "Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
-
                 # State should be stored in this dictionary
                 state = self.state[p]
+                if len(state) == 0:  # Initialize parameter state
+                    state['m_t'] = torch.zeros_like(grad)
+                    state['v_t'] = torch.zeros_like(grad)
+                    state['t'] = 0
+                state['t'] += 1
 
                 # TODO: Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
+                beta1, beta2 = group['betas']
+                eps = group['eps']
+                lam = group['weight_decay']
+                correct_bias = group['correct_bias']
 
                 # TODO: Update first and second moments of the gradients
+                state['m_t'].mul_(beta1).add_(grad, alpha=1-beta1)
+                state['v_t'].mul_(beta2).addcmul_(grad, grad, value=1-beta2)
 
                 # TODO: Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
+                if correct_bias:
+                    alpha *= (1 - beta2**state['t']) ** 0.5 / (1 - beta1**state['t'])
 
                 # TODO: Update parameters
+                p.data.addcdiv_(state['m_t'], state['v_t']**0.5+eps, value=-alpha)
 
                 # TODO: Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
+                p.data.mul_(1 - alpha * lam)
 
         return loss
